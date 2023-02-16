9eafafafac76a2317bdb76931c1243324542c96f
Ванеев Денис <vaneev.denis98@ya.ru>
2023-02-16 16:05:33 +0500

    Check_x2

--- lol.py
+++ lol.py
@@ -1,21 +1,4 @@
-from math import sqrt
+def avg(first_num, second_num):
+    return first_num + second_num / 2
 
-message = 'Добро пожаловать в самую лучшую программу для вычисления ' \
-          'квадратного корня из заданного числа'
-print(message)
-
-
-def CalculateSquareRoot(Number):
-    """Вычисляет квадратный корень."""
-    return sqrt(Number)
-
-
-def calc(your_number):
-    if your_number <= 0:
-        print('Из заданного числа не существует квадратног окорня')
-    else: 
-        print(f'Мы вычислили квадратный корень из введённого вами числа.'
-              f'Это будет: {CalculateSquareRoot(your_number)}')
-
-
-calc(-2)
+print(avg(5, 10)) 
